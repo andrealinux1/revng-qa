2742b6b14831072b2cc7bc2839fc2a648bed3553
Andrea Gussoni <andreagus@rev.ng>
2024-05-02 17:29:45 +0200

    Improve `switch` promotion test case
    Modify the original source code for the test in order to enforce the
    introduction of an exit dispatcher.
    
    Comment the structure of the source code of the test in order to
    simplify the understanding of it.

--- share/revng/test/tests/analysis/Decompilation/x86-64/dual-case-switch.c
+++ share/revng/test/tests/analysis/Decompilation/x86-64/dual-case-switch.c
@@ -7,18 +7,41 @@
 WEAK void report(int code) {
 }
 
+WEAK int random(int arg) {
+  return arg;
+}
+
+/// With this function we want to enforce the creation of an exit dispatcher
+/// with two `case`s following the `while (arg1)` loop. The `switch` to `if`
+/// beautify pass, should then create operate in order to reconstruct an `if`.
+
 WEAK int f(int arg1) {
   int return_value;
+
+  if (arg1 == 50) {
+
+  // This forward edge is needed in order to avoid the inlining the blocks
+  // following the loop inside the region representing the loop (early successor
+  // inlining)
+    goto LABEL_EXIT_1;
+  }
+
   while (arg1 > 0) {
     if (arg1 == 1) {
       return_value = 10;
       report(1);
+
+      // This `goto` is used in order to reach one of the successors of the loop
       goto LABEL_EXIT_1;
     } else if (arg1 == 2) {
       return_value = 20;
       report(2);
+
+      // This `goto` is used in order to reach the other successor of the loop
       goto LABEL_EXIT_2;
     }
+    report(arg1);
+    arg1 = random(arg1);
   }
 
 LABEL_EXIT_1:
